0cccc6398056ae6cdf8c935102b8a18fc480feec
Tomas Castro Rojas <tomascastrorojas18@gmail.com>
2021-09-18 00:57:05 -0300

    alinear lets, funciones infijas

--- src/Eval1.hs
+++ src/Eval1.hs
@@ -63,7 +63,7 @@ evalExp (Times exp1 exp2) s = let (n1 :!: s') = evalExp exp1 s
                               in (n1 * n2 :!: s'')
 evalExp (Div exp1 exp2) s = let (n1 :!: s') = evalExp exp1 s
                                 (n2 :!: s'') = evalExp exp2 s'
-                            in (n1 'div' n2 :!: s'')
+                            in (div n1 n2 :!: s'')
 evalExp (EAssgn var exp) s = let (n :!: s') = evalExp exp s
                              in (n :!: update var n s')
 evalExp (ESeq exp1 exp2) s = let (_ :!: s') = evalExp exp1 s
@@ -78,16 +78,16 @@ evalExp (Gt exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
                                (b2 :!: s'') = evalExp exp2 s'
                            in (b1 > b2 :!: s'')
 evalExp (And exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
-                               (b2 :!: s'') = evalExp exp2 s'
-                           in (b1 && b2 :!: s'')
+                                (b2 :!: s'') = evalExp exp2 s'
+                           in ((b1 && b2) :!: s'')
 evalExp (Or exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
                                (b2 :!: s'') = evalExp exp2 s'
-                           in (b1 || b2 :!: s'')
+                           in ((b1 || b2) :!: s'')
 evalExp (Not exp) s = let (b :!: s') = evalExp exp s
                       in (not b :!: s')
 evalExp (Eq exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
                                (b2 :!: s'') = evalExp exp2 s'
                            in (b1 == b2 :!: s'') 
 evalExp (NEq exp1 exp2) s = let (b1 :!: s') = evalExp exp1 s
-                               (b2 :!: s'') = evalExp exp2 s'
-                           in (b1 /= b2 :!: s'')
+                                (b2 :!: s'') = evalExp exp2 s'
+                            in (b1 /= b2 :!: s'')
